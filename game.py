1d7bc27219df8c7ddc74e3f51c8bbdfddd6234bd
Cho-Bogeun <bog9989@gmail.com>
2025-07-31 14:24:17 +0900

    [refactoring] page49

--- game.py
+++ game.py
@@ -1,14 +1,19 @@
 class Game:
     def guess(self, numbers):
+        self._assert_illegal_value(numbers)
+
+    def _assert_illegal_value(self, numbers):
         if numbers is None:
-            raise TypeError()
-
+            raise TypeError("입력이 없음")
+        
         if len(numbers) != 3:
-            raise TypeError
+            raise TypeError("세글자가 아님")
+        
+        if not numbers.isdigit():
+            raise TypeError("숫자가 아님")
+            
+        if self._does_duplicated(numbers):
+            raise TypeError("중복 숫자 존재")
 
-        for number in numbers:
-            if not ord("0") <= ord(number) <= ord("9"):
-                raise TypeError
-
-        if numbers[0] == numbers[1] or numbers[1] == numbers[2] or numbers[0] == numbers[2]:
-            raise TypeError
+    def _does_duplicated(self, numbers):
+        return len(set(numbers)) != 3
